830993b1f481850ec1d70a008926dc18368dcadc
falcon <dhruvparashar6@gmail.com>
2017-08-29 04:52:27 +0530

    added classifier

--- analysis.py
+++ analysis.py
@@ -1,10 +1,10 @@
 import twitter 
 
 
-api = twitter.Api(consumer_key='4Mpcp4aZdh0F2VS7nPHE6dgBC',
-	consumer_secret='eMkStwK4C4LCdCspt92Rg5MS2OedEJKjJzVtnhgKatHhEAq34e',
-	access_token_key='4358774233-p8P1IVzt7lJGvROi5aWUjlO3hPbKvC05UIdixH3',
-	access_token_secret='IS6b8Dv19s6Hh6EBc3BZoahQ3uNqZXmn9rjlzxgtC2Fcf')
+api = twitter.Api(consumer_key=' ',
+	consumer_secret=' ',
+	access_token_key=' ',
+	access_token_secret=' ')
 
 
 
